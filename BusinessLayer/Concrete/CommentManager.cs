78993ecc9748e3b5a871b8637c108c0d2ffc117b
furkan-ermis <furkan.ermis@icloud.com>
2023-06-20 20:06:37 +0300

    AdminComment

--- BusinessLayer/Concrete/CommentManager.cs
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -11,7 +11,7 @@ namespace BusinessLayer.Concrete
 {
     public class CommentManager : ICommentService
     {
-         ICommentDal _commentDal; 
+        ICommentDal _commentDal;
         public CommentManager(ICommentDal commentDal)
         {
             _commentDal = commentDal;
@@ -24,26 +24,31 @@ namespace BusinessLayer.Concrete
 
         public void TDelete(Comment t)
         {
-            throw new NotImplementedException();
+            _commentDal.Delete(t);
         }
 
         public Comment TGetByID(int id)
         {
-            throw new NotImplementedException();
+            return _commentDal.GetById(id);
         }
 
         public List<Comment> TGetList()
         {
-            throw new NotImplementedException();
+            return _commentDal.GetList();
         }
         public List<Comment> TGetDestinationByID(int id)
         {
-            return _commentDal.GetListByFilter(x=>x.DestinationID == id);
+            return _commentDal.GetListByFilter(x => x.DestinationID == id);
         }
 
         public void TUpdate(Comment t)
         {
             throw new NotImplementedException();
+        }
+
+        public List<Comment> TGetListCommentWithDestination()
+        {
+            return _commentDal.GetListCommentWithDestination();
         }
     }
 }
